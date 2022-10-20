09e617d6eba25ca397dc53f8903cec7a3b88a049
sureshd-8451 <94718959+sureshd-8451@users.noreply.github.com>
2022-10-20 14:49:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,9 @@ variable "location" {
 
 locals {
   base_name = "${var.prefix}web"
-  change = "trigger-08032022"
+  #change = "trigger-08032022"
+  change = "trigger a change"
+  
 }
 
 resource "azurerm_resource_group" "web" {
